0cb36ed890b43c6c9002253261ff4c5dded9283f
aa195113 <abderrahmane.abaakil@etu.univ-lehavre.fr>
2023-12-05 15:49:48 +0100

    Modifications des routes dans App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -3,10 +3,10 @@ import { StripeProvider } from '@stripe/stripe-react-native';
 import Constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CheckoutScreen from './CheckoutScreen';
-import LandingPage from './LandingPage';
-import Historique from './HistoriqueScreen';
-import Panier from './PanierScreen';
+import CheckoutScreen from './Views/CheckoutScreen';
+import LandingPage from './Views/LandingPage';
+import Historique from './Views/HistoriqueScreen';
+import Panier from './Views/PanierScreen';
 
 const Stack = createNativeStackNavigator();
 
