b5c5df7f80b80e44b83226f23161717818f52b7c
aa195113 <abderrahmane.abaakil@etu.univ-lehavre.fr>
2023-12-06 18:23:25 +0100

    Implement Panier functionalities plus update CheckoutScreen

--- client/Views/CheckoutScreen.tsx
+++ client/Views/CheckoutScreen.tsx
@@ -1,138 +1,132 @@
 import { useStripe } from "@stripe/stripe-react-native";
 import Constants from "expo-constants";
 import React, { useEffect, useState } from "react";
-import { Alert, Text, Button, SafeAreaView, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Alert, Text, Button, SafeAreaView, View } from "react-native";
+import { db } from "../db";
 
-export default function CheckoutScreen() {
-    const { initPaymentSheet, presentPaymentSheet } = useStripe();
-    const [loading, setLoading] = useState(false);
-    const [paymentIntentId, setPaymentIntentId] = useState<string>("");
+export default function CheckoutScreen({ items, clearCart, total }) {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [loading, setLoading] = useState(false);
+  const [paymentIntentId, setPaymentIntentId] = useState<string>("");
 
-    const apiUrl = Constants.expoConfig.extra.apiUrl;
+  const apiUrl = Constants.expoConfig.extra.apiUrl;
+  const userId = "cus_OsUYRe24A3emkb";
+  console.log("items :", items);
+  const itemss = [
+    {
+      id: 1,
+      amount: 100,
+    },
+  ];
 
-    const userId = "cus_OsUYRe24A3emkb";
-    const items = [
+  const fetchPaymentSheetParams = async () => {
+    const response = await fetch(`${apiUrl}/payments/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        pending_items: items,
+        customer_id: userId,
+      }),
+    });
+
+    const { paymentIntent, ephemeralKey, customer } = await response.json();
+
+    return {
+      paymentIntent,
+      ephemeralKey,
+      customer,
+    };
+  };
+
+  const initializePaymentSheet = async () => {
+    const { paymentIntent, ephemeralKey, customer } =
+      await fetchPaymentSheetParams();
+
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      paymentIntentClientSecret: paymentIntent,
+      allowsDelayedPaymentMethods: false,
+    });
+
+    if (!error) {
+      setPaymentIntentId(paymentIntent);
+      setLoading(true);
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      const paymentIntent = `pi_${paymentIntentId.split("_")[1]}`;
+      const response = await fetch(
+        `${apiUrl}/payments/check/${paymentIntent}`,
         {
-            "id": 1,
-            "amount": 2
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            customer_id: userId,
+          }),
         }
-    ];
-
-    const fetchPaymentSheetParams = async () => {
-        const response = await fetch(`${apiUrl}/payments/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "pending_items": items,
-                "customer_id": userId
-            })
-        });
-
-        const { paymentIntent, ephemeralKey, customer } = await response.json();
-
-        return {
-            paymentIntent,
-            ephemeralKey,
-            customer,
-        };
-    };
-
-    const initializePaymentSheet = async () => {
-        const {
-            paymentIntent,
-            ephemeralKey,
-            customer,
-        } = await fetchPaymentSheetParams();
-
-        const { error } = await initPaymentSheet({
-            merchantDisplayName: "Example, Inc.",
-            customerId: customer,
-            customerEphemeralKeySecret: ephemeralKey,
-            paymentIntentClientSecret: paymentIntent,
-            allowsDelayedPaymentMethods: false,
-        });
-
-        if (!error) {
-            setPaymentIntentId(paymentIntent);
-            setLoading(true);
-        }
-    };
-
-    const openPaymentSheet = async () => {
-        const { error } = await presentPaymentSheet();
-
-        if (error) {
-            Alert.alert(`Error code: ${error.code}`, error.message);
-        } else {
-            const paymentIntent = `pi_${paymentIntentId.split("_")[1]}`;
-            const response = await fetch(`${apiUrl}/payments/check/${paymentIntent}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    "customer_id": userId
-                })
-            });
-
-            if (response.status == 200) Alert.alert('Success', 'Your order is confirmed!');
-        }
-    };
-
-    useEffect(() => {
-        initializePaymentSheet();
-    }, []);
-
-   return (
-          <SafeAreaView style={styles.container}>
-              <View style={styles.topContent}>
-                  <Text style={styles.paymentText}>Payment</Text>
-              </View>
-              <View style={styles.bottomContent}>
-                  {/* Your content here */}
-              </View>
-              <TouchableOpacity
-                  style={styles.checkoutButton}
-                  disabled={!loading}
-                  onPress={openPaymentSheet}
-              >
-                  <Text style={styles.buttonText}>Checkout</Text>
-              </TouchableOpacity>
-          </SafeAreaView>
       );
-  }
 
-  const styles = StyleSheet.create({
-      container: {
-          flex: 1,
-      },
-      topContent: {
-          flex: 0.2,
-          justifyContent: 'center', // Center the text vertically
-          alignItems: 'center', // Center the text horizontally
-      },
-      bottomContent: {
-          flex: 1,
-          justifyContent: 'center', // Center your content vertically
-          alignItems: 'center', // Center your content horizontally
-      },
-      paymentText: {
-          fontSize: 30,
-      },
-      checkoutButton: {
-          backgroundColor: '#3a86ff',
-          marginBottom: 10,
-          marginLeft:5,
-          marginRight:5,
-          padding: 10,
-          borderRadius: 5,
-          alignItems: 'center',
-          justifyContent: 'center',
-      },
-      buttonText: {
-          color: 'white',
-          fontSize: 18,
-      },
-  });
+      if (response.status == 200) {
+        const currentDate = new Date(); // Get the current date and time
+        const formattedDate = currentDate.toISOString();
+        Alert.alert("Success", "Your order is confirmed!");
+        clearCart();
+        db.transaction(
+          (tx) => {
+            tx.executeSql(
+              "INSERT INTO Achats (purchaseDate, price) VALUES (?, ?)",
+              [formattedDate, total],
+              (_, result) => {
+                console.log("Data inserted successfully into Achats table.");
+              },
+              (error) => {
+                console.error("Error inserting data into Achats table:", error);
+              }
+            );
+          },
+          (error) => {
+            console.error("Transaction Error", error);
+          }
+        );
+      }
+    }
+  };
+
+  useEffect(() => {
+    initializePaymentSheet();
+  }, []);
+
+  return (
+    <SafeAreaView>
+      {!loading ? (
+        <Text style={styles.text}>loading</Text>
+      ) : (
+        <Text
+          style={styles.text}
+          disabled={!loading}
+          onPress={openPaymentSheet}
+        >
+          Checkout
+        </Text>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = {
+  text: {
+    color: "white",
+  },
+};
--- client/Views/PanierScreen.tsx
+++ client/Views/PanierScreen.tsx
@@ -1,11 +1,377 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, Image, FlatList, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { db } from "../db";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import CheckoutScreen from "./CheckoutScreen";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import Constants from "expo-constants";
+import { useFocusEffect } from "@react-navigation/native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function PanierScreen() {
+const stripePK = Constants.expoConfig.extra.stripePK;
+type Item = {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+function PanierItem({
+  item,
+  updateQuantityMinus,
+  updateQuantityPlus,
+}: {
+  item: Item;
+  updateQuantityMinus: (itemId: number, itemPrice: number) => void;
+  updateQuantityPlus: (itemId: number, itemPrice: number) => void;
+}) {
+  const { containerStyle, textStyle, items, Prix, itemStyle } =
+    styles;
+
   return (
-    <View>
-      <Text>Panier (Cart) Screen</Text>
-      {/* Display the cart items here */}
+    <View style={containerStyle}>
+
+
+      <View style={items}>
+        <Icon.Button
+          name="ios-remove"
+          size={15}
+          color="#fff"
+          backgroundColor="rgba(255, 255, 255, 0)"
+          style={{
+            borderRadius: 15,
+            backgroundColor: "red",
+            height: 30,
+            width: 30,
+          }}
+          iconStyle={{ marginRight: 0 }}
+          onPress={() => {
+            updateQuantityMinus(item.id, item.price);
+          }}
+        />
+
+      <View style={textStyle}>
+      <View style={itemStyle}>
+        <Text >{item.name}  :   {item.quantity}</Text>
+        <View style={Prix}>
+          <Text style={styles.price}>${item.price}</Text>
+        </View>
+        </View>
+      </View>
+        <Icon.Button
+          name="ios-add"
+          size={15}
+          backgroundColor="rgba(255, 255, 255, 0)"
+          style={{
+            color: styles.darkText,
+            borderRadius: 15,
+            backgroundColor: "green",
+            height: 30,
+            width: 30,
+          }}
+          iconStyle={{ marginRight: 0 }}
+          onPress={() => {
+            updateQuantityPlus(item.id, item.price);
+          }}
+        />
+      </View>
     </View>
   );
 }
+
+export default function Panier() {
+  const [cartItems, setCartItems] = useState<Item[]>([]);
+  const [total, setTotal] = useState(0);
+  const [showCheckout, setShowCheckout] = useState(false);
+
+  const [checkoutTotalJson, setCheckoutTotalJson] = useState<any[]>([
+    { id: 1, amount: 100 },
+  ]);
+
+  useEffect(() => {
+    if (checkoutTotalJson.length > 0) {
+      setShowCheckout(true);
+    }
+  }, [checkoutTotalJson]);
+
+  useEffect(() => {
+    setShowCheckout(false);
+    const newCheckoutTotalJson = cartItems.map((item) => ({
+      id: item.itemId,
+      amount: item.quantity * 100,
+    }));
+
+    setCheckoutTotalJson(newCheckoutTotalJson);
+  }, [cartItems]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      db.transaction(
+        (tx) => {
+          try {
+            tx.executeSql("SELECT * FROM Panier", [], (_, { rows }) => {
+              const cartData = rows._array;
+              setCartItems(cartData);
+              console.log("Data Fetched Successfully", cartItems);
+              const initialTotal = cartData.reduce(
+                (acc, item) => acc + item.price * item.quantity,
+                0
+              );
+              setTotal(initialTotal);
+            });
+          } catch (error) {
+            console.error("Error while executing SQL:", error);
+          }
+        },
+        (error) => {
+          console.error("Transaction error:", error);
+        }
+      );
+    }, [])
+  );
+
+ const Vider = () => {
+    db.transaction(
+      (tx) => {
+        // Drop the existing panier table
+        tx.executeSql(
+          "DROP TABLE IF EXISTS Panier",
+          [],
+          () => {
+            console.log("Cart table dropped.");
+
+            // Recreate the panier table with the same structure
+            tx.executeSql(
+              "CREATE TABLE IF NOT EXISTS Panier (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, itemId INTEGER, quantity INTEGER)",
+              [],
+              () => {
+                console.log("Cart table recreated.");
+
+                setCartItems([]); // Clear the local state
+              },
+              (error) => {
+                console.error("Error recreating cart table:", error);
+              }
+            );
+          },
+          (error) => {
+            console.error("Error dropping cart table:", error);
+          }
+        );
+      },
+      (error) => {
+        console.error("Transaction Error", error);
+      }
+    );
+  };
+
+
+  const updateQuantityMinus = (itemId: number, itemPrice: number) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE Panier SET quantity = quantity - 1 WHERE itemId = ?",
+        [itemId],
+        (_, result) => {
+          const updatedItems = cartItems.map((item) => {
+            if (item.itemId === itemId) {
+              if (item.quantity - 1 === 0) {
+                tx.executeSql(
+                  "DELETE FROM Panier WHERE itemId = ?",
+                  [itemId],
+                  () => {
+                    console.log("Item removed from the database.");
+                  },
+                  (error) => {
+                    console.error(
+                      "Error removing item from the database:",
+                      error
+                    );
+                  }
+                );
+              }
+              return {
+                ...item,
+                quantity: item.quantity - 1,
+              };
+            }
+            return item;
+          });
+          setTotal(total - itemPrice); // Update the total price
+          setCartItems(updatedItems);
+        },
+        (error) => {
+          console.error("Error decreasing quantity:", error);
+        }
+      );
+    });
+  };
+
+  const updateQuantityPlus = (itemId: number, itemPrice: number) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE Panier SET quantity = quantity + 1 WHERE itemId = ?",
+        [itemId],
+        (_, result) => {
+          // Update the local state with the updated cart items
+          const updatedItems = cartItems.map((item) => {
+            if (item.itemId === itemId) {
+              return {
+                ...item,
+                quantity: item.quantity + 1,
+              };
+            }
+            return item;
+          });
+          setCartItems(updatedItems);
+          setTotal(total + itemPrice); // Update the total price
+        },
+        (error) => {
+          console.error("Error increasing quantity:", error);
+        }
+      );
+    });
+  };
+
+  return (
+    <>
+      {cartItems.length <= 0 ? (
+        <View style={styles.ItemEx}>
+          <AntDesign name="exclamationcircle" size={35} color="gray" />
+          <Text style={styles.NoItem}>No Item Added </Text>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <View style={styles.itemContainer}>
+            <FlatList
+              data={cartItems}
+              renderItem={({ item, index }) => (
+                <PanierItem
+                  item={item}
+                  index={index}
+                  updateQuantityMinus={updateQuantityMinus}
+                  updateQuantityPlus={updateQuantityPlus}
+                />
+              )}
+              keyExtractor={(item) => item.id.toString()}
+            />
+          </View>
+          <View >
+            <View style={styles.sum}>
+              <MaterialCommunityIcons name="cart-variant" size={24} color="black" />
+                <Text>${total}</Text>
+            </View>
+            <View style={styles.check}>
+              <View style={styles.checkoutButtonStyle}>
+                {!showCheckout ? (
+                  <Text>none</Text>
+                ) : (
+                  <StripeProvider
+                    publishableKey={stripePK}
+                    merchantIdentifier="merchant.com.example"
+                  >
+                    <CheckoutScreen
+                      items={checkoutTotalJson}
+                      total={total}
+                    />
+                  </StripeProvider>
+                )}
+              </View>
+              {cartItems.length > 0 && (
+                 <TouchableOpacity
+                       style={styles.clearCartButton}
+                       onPress={() => Vider()}
+                     >
+                       <Text>Clear Cart</Text>
+                     </TouchableOpacity>
+                )}
+            </View>
+          </View>
+        </View>
+      )}
+    </>
+  );
+}
+
+const styles = {
+  container: {
+    flex: 0.91,
+  },
+  itemContainer: {
+    flex: 10,
+  },
+  itemStyle : {
+  alignItems : "center",
+  },
+  containerStyle: {
+    flexDirection: "row",
+    flex: 1,
+    borderBottomWidth: 1,
+    borderColor: "white",
+    padding: 10,
+    paddingLeft: 15,
+    backgroundColor: "#fefae0",
+  },
+   clearCartButton: {
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#e74c3c",
+      padding: 10,
+      borderRadius: 5,
+      marginTop: 15,
+    },
+  NoItem: {
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 50,
+    color: "gray",
+  },
+  ItemEx: {
+    marginTop: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+    color: "gray",
+  },
+  lastItemStyle: {
+    flexDirection: "row",
+    flex: 1,
+    padding: 10,
+    paddingLeft: 15,
+    backgroundColor: "#fff",
+  },
+  textStyle: {
+    flex: 2,
+    justifyContent: "center",
+  },
+  price: {
+    color: "white",
+    fontSize: 13,
+  },
+  Prix: {
+    backgroundColor: "#d4a373",
+    width: 40,
+    alignItems: "center",
+    marginTop: 3,
+    borderRadius: 3,
+  },
+  items: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  checkoutButtonStyle: {
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor: "#ccd5ae",
+    padding: 10,
+    borderRadius: 5,
+    marginTop:10
+  },
+  sum: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: 15,
+  },
+};
